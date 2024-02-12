7eb703d199ddbcfe56572493bbce084c44b12100
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2024-02-12 06:53:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import base64
 
 def change_profile_picture(token, image_path):
     try:
+        image_path = image_path.replace('"',"")
         with open(image_path, "rb") as image_file:
             encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
-        
         headers = {
             "Authorization": f"Bot {token}",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
@@ -16,15 +16,14 @@ def change_profile_picture(token, image_path):
         }
         url = "https://discord.com/api/v9/users/@me"
         response = requests.patch(url, headers=headers, json=data)
-        print('Sucess! Profile Picture Added !')
+        print('Success! Profile Picture Added!')
     except Exception as e:
         print(f"An error occurred: {e}")
 
 def main():
     token = input("Enter your bot token: ")
-    gif_path = input("Enter the path to your GIF file: ")
-    change_profile_picture(token, gif_path)
+    image_path = input("Drag and drop your GIF file here and press Enter: ")
+    change_profile_picture(token, image_path)
 
 if __name__ == "__main__":
     main()
-    input('press any key to quit....')
